06155bb7cc133f3b55f29747b28b911c7a94bac1
noazaj <noahzajicek@gmail.com>
2024-08-02 12:07:26 -0400

    Update main for procfile

--- app/web/main.py
+++ app/web/main.py
@@ -33,6 +33,5 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True,
-            host=os.getenv('FLASK_RUN_HOST'),
-            port=os.getenv('FLASK_RUN_PORT'))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
